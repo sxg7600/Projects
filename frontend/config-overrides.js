6b663fb36b3a0929a59fca7f0bc9953ed2cee25a
Sajan Gautam <112038429+sajangautam@users.noreply.github.com>
2024-07-16 15:17:24 -0500

    Delete frontend directory

--- frontend/config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { override, addBabelPlugin } = require('customize-cra');
-
-module.exports = override(
-  addBabelPlugin('babel-plugin-macros'),
-  addBabelPlugin('@babel/plugin-syntax-optional-chaining')
-);
--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello from the frontend!"));
-
-app.listen(port, () => console.log(`Frontend app listening at http://localhost:${port}`));
